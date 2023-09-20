cbbe2fcd0d9fa467efcfbb3f3f6face496cd0e4b
charlie <linkin8288@gmail.com>
2023-09-20 16:10:55 +0200

    Add Navbar and Routes

---
+++ app/(dashboard)/(routes)/analytics/page.tsx
@@ -0,0 +1,11 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+function AnalyticsPage() {
+  return (
+    <div>AnalyticsPage</div>
+  )
+}
+
+export default AnalyticsPage
---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,9 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+export default function CoursesPage() {
+  return (
+    <div className="p-6">CoursesPage</div>
+  )
+}
