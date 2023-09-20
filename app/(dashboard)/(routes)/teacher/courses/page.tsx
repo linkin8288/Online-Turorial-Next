f7e068a9c4e9de8c0c5a74d59d229709ad966bee
charlie <linkin8288@gmail.com>
2023-09-20 16:54:45 +0200

    Add Course Form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,17 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
 
 export default function CoursesPage() {
   return (
-    <div className="p-6">CoursesPage</div>
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>
+          New Course
+        </Button>
+      </Link>
+      
+    </div>
   )
 }
